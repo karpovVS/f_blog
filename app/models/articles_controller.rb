8b6c967212c2e1e26693318781e26d98eaaaa9b3
karpovVS <kazankarpov@gmail.com>
2019-11-25 21:52:33 +0300

    Fix create cntrl,create show controller, add flash and errors msgs git push

--- app/models/articles_controller.rb
+++ app/models/articles_controller.rb
@@ -6,9 +6,18 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to_articles_show(@article)
-	end	
+		if @article.save
+			flash[:notice] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+	  end
+	
+	def show
+		@article = Article.find(params[:id])
+	end
+		
 
 	private
 	def article_params
